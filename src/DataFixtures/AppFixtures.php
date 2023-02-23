c8dee20a2c37d09d80bacecb9f2d7ef9ea520b31
Lynda Benabdessadok <lylyben74@gmail.com>
2023-02-23 09:19:29 +0100

    create entities

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,8 +9,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product = new Product();
+        $manager->persist($product);
 
         $manager->flush();
     }
