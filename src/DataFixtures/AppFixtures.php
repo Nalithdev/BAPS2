ac8602226ae2179043414c6f83e0e02763090bce
Lynda Benabdessadok <lylyben74@gmail.com>
2023-02-23 10:15:53 +0100

    pull merge

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,7 +10,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $product = new Product();
+        $product = new User();
         $manager->persist($product);
 
         $manager->flush();
