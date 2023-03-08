fba280c5b80ddaf77e27462f23f2f31f24507ca8
Lynda Benabdessadok <lylyben74@gmail.com>
2023-03-08 14:49:37 +0100

    tokens

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User ;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class AuthController extends AbstractController
+{
+    #[Route('/auth1', name: 'app_auth1', methods: ['POST'])]
+    public function auth(Request $request, UserRepository $userRepository ): Response
+    {
+        // Je crée une instance User dans laquelle je lui demande de chercher les id des utilisateurs
+        $users = $userRepository->findAll();
+
+
+        // générer le token
+        $token = new UsernamePasswordToken($users, null, $users->getRoles());
+
+
+        // stocker le jeton généré dans la session
+        $session = $request->getSession();
+        if ($session) {
+            $session->set('_security_main', serialize($token));
+        }
+
+        // retourner le token
+        return $this->json(['token' => $token]);
+    }
+}
