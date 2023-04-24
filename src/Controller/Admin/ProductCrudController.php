3d30b560d1f5cb04bcedc92755f21d2c06d6ee2a
TuberculeP <lavieville.felix@gmail.com>
2023-04-24 19:33:23 +0200

    Modifying Crud

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -2,36 +2,32 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Category;
-use App\Entity\Commerce;
+use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
-class CommerceCrudController extends AbstractCrudController
+class ProductCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Commerce::class;
+        return Product::class;
     }
 
     public function configureActions(Actions $actions): Actions
     {
         return $actions->remove(Crud::PAGE_INDEX, Action::NEW);
     }
+
+    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('name')->setLabel('Nom'),
-            TextField::new('description'),
-			AssociationField::new('owner')->setLabel('Propriétaire'),
-			TextField::new('adresse'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
         ];
     }
+    */
 }
